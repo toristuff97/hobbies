5a49243f3590f64b6ba50e2909a5e1c59726b2d6
Victoria Jones <victoriajones@psadmins-air-3.lan>
2020-07-13 19:33:24 -0400

    changes to hobbies.js

--- src/components/Hobbies.js
+++ src/components/Hobbies.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 class Hobbies extends Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+            hobbyName: "Roller",
+            description: "",
+            image: ""
+        }
+    }
+
+
+    render() {
+
+        const skate = {hobbyName: "Rollerskating", description: "Rollerskating is the act of riding around on shoes with 4 wheels on each shoe and doing tricks and dances"}
+        return (
+            <div>
+            <div className="title">
+                <h2>My favorite hobby is Rollerskating.</h2>
+            </div>
+
+            <div className="skating">
+                <h3>Hobby Name:</h3>
+                <h3>Image:</h3>
+                <h3>Hobby Description:</h3>
+            </div>
+
+            <div className="cooking">
+                <h3>Hobby Name:</h3>
+                <h3>Image:</h3>
+                <h3>Hobby Description:</h3>
+            </div>
+
+            <div className="gaming">
+                <h3>Hobby Name:</h3>
+                <h3>Image:</h3>
+                <h3>Hobby Description:</h3>
+            </div>
+        </div>
+        )
+    }
 }
 
 export default Hobbies;
